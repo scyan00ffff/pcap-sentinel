9d7b03524b6f889b3ca599bd053dfaced066ad67
scyan00ffff <reubenjsykes@gmail.com>
2026-05-16 01:03:59 +0100

    continued improvements to flask webpage

--- report.py
+++ report.py
@@ -6,7 +6,11 @@ findings_data = []
 
 @app.route("/")
 def dashboard():
-    return render_template("dashboard.html", findings = findings_data)
+    info_findings = [f for f in findings_data if f['Severity'] == 'INFO']
+    high_findings = [f for f in findings_data if f['Severity'] != 'INFO']
+    return render_template("dashboard.html",
+                            findings = high_findings,
+                            info_findings=info_findings)
 
 def start_server(findings):
     global findings_data
